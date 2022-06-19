e067c2f587d89a2aa2668984e8621efc9c02c0c4
mralibatu <alibatuhan404@gmail.com>
2022-06-19 10:25:00 +0300

    yorum eklenmesi

--- src/Giris.java
+++ src/Giris.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 public class Giris {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String gkullaniciadi,gsifre;
-        String kullaniciadi = "mralibatu", sifre ="404";
+        String gkullaniciadi,gsifre;                //Kullanıcının gireceği değerler
+        String kullaniciadi = "mralibatu", sifre ="404";    //doğru olan değerler
+
         System.out.println("Hosgeldiniz!\n" +"Lutfen kullanici adi giriniz :");
         gkullaniciadi = input.nextLine();
         System.out.println("Lutfen sifre giriniz :");
         gsifre = input.nextLine();
 
         if (gkullaniciadi.equals(kullaniciadi) && (gsifre.equals(sifre))){
+            /*
+                equals ile gkullaniciadi ve kullaniciadi ni karsılastırıyoruz
+                && ise hem kullanıcı adının hem sifrenin dogrusunu istedigimizden kullanıyoruz.
+            */
             System.out.println("Basariyla giris yaptiniz!");
         }else {
             System.out.println("Bilgileriniz yanlis, sifrenizi sifirlamak isterseniz 1 e basin");
-            int x = input.nextInt();
 
+            int x = input.nextInt();        /*sıfırlayıp isteyip istemedigini ogrenmek icin bir x degiskeni atayıp
+                                     bunun dogru olup olmadıgına bakacaz case ile       */
             switch(x) {
                 case 1:
                     System.out.println("Yeni sifrenizi giriniz : ");
@@ -24,7 +30,8 @@ public class Giris {
                     System.out.println("Sıfre degistirilmek istenmedi");
             }
             gsifre = input.nextLine();
-            if (gsifre.equals(sifre) && x == 1){
+            if (gsifre.equals(sifre) && x == 1){            /*sifrenin onceki sifreyle aynı olup olmadıgını kontrol ediyoruz
+                                                        x==1 kullanımının sebebi eger sifre degistirme talebi yoksa if e girmemesi */
                 System.out.println("Girdiginiz sifre onceki sifre ile ayni, tekrar deneyin");
                 gsifre = input.nextLine();
             } else if (x != 1) {
